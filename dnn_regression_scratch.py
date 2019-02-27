e918f68886d6f053178b6af15d41993c95520324
MonaBabikir <Mona.Mohamedelamin.6258@student.uu.se>
2019-02-27 11:40:18 +0100

    adding training example for other problem

--- dnn_regression_scratch.py
+++ dnn_regression_scratch.py
@@ -18,10 +18,14 @@ class NeuralNetwork:
         self.y          = y
         self.output     = np.zeros(y.shape)
 
+        #print(self.input.shape, "\n", self.weights1.shape ,"\n",self.weights2.shape,"\n",self.output.shape,"\n")
+
 
     def feedforward(self):
         self.layer1 = sigmoid(np.dot(self.input, self.weights1))
         self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
+        #print(self.input.shape, "\n", self.weights1.shape, "\n", self.weights2.shape, "\n", self.output.shape, "\n")
 
     def backprop(self):
         # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
@@ -36,14 +40,23 @@ class NeuralNetwork:
     def cost(self, dataset,labels):
         error = 0.0
         for xi,yi in zip(dataset, labels) :
-            self.input = xi
-            self.y = yi
-            self.feedforward()
-            error += math.pow((self.y - self.output),2)
+            # self.input = xi
+            # self.y = yi
+            # self.feedforward()
+            prediction = self.predict(xi)
+            # print("pre = ",prediction)
+            # print("y = ", yi)
+            error += math.pow((yi - prediction),2)
         return error
 
-x = np.random.rand(1,3)
-y = np.random.rand(1)
+    def predict(self,x):
+        o1 = sigmoid(np.dot(x, self.weights1))
+        o2 = sigmoid(np.dot(o1, self.weights2))
+
+        return o2
+
+#x = np.random.rand(1,3)
+#y = np.random.rand(1)
 #print(x,y)
 #model = NeuralNetwork(x,y)
 #print(model, model.weights1, model.weights2, model.output)
@@ -56,8 +69,8 @@ x, y = loadlocal_mnist(
         labels_path='../MNIST Data Set/train-labels.idx1-ubyte') ## data set downloaded from 'http://yann.lecun.com/exdb/mnist/'
 
 # print(x[0],y[0])
-# shape x = 60000 * 784 = 60000 training example/input, 784 features
-epoch = 5
+# shape x = 60000 * 784 , 60000 training example/input, 784 features
+epoch = 1000
 def training(trainset, labels):
     print(np.array(trainset[0]).T.shape)
     model = NeuralNetwork(np.reshape(trainset[0],(1,784)), np.array([labels[0]]))
@@ -81,8 +94,24 @@ def training(trainset, labels):
 # print(x[0].shape, z.T.shape)
 # w = np.array([y[0]])
 # print(w.shape)
-a = np.reshape(x[0],(28,28))
-print(a)
 
-training(x,y)
+#training(x,y)
 
+x = np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1]])
+y = np.array([[0],[1],[1],[0]])
+#model = NeuralNetwork(x, y)
+model = NeuralNetwork(np.array([[0,0,1]]), np.array([[0]]))
+def training_xor(trainset,lables):
+    for i in range(epoch):
+        for xi, yi in zip(trainset, lables):
+            model.input = np.array([xi])
+            model.y = np.array([yi])
+            model.feedforward()
+            model.backprop()
+        print("for this iteration error =  ", model.cost(trainset, lables))
+
+training_xor(x,y)
+print("the prediction of 1,1,1 = ",model.predict(np.array([1,1,1])))
+print("the prediction of 0,0,1 = ",model.predict(np.array([0,0,1])))
+print("the prediction of 1,0,1 = ",model.predict(np.array([1,0,1])))
+print("the prediction of 0,1,1 = ",model.predict(np.array([0,1,1])))
