4df70a96f0db35d902017b98b0d364510707eb67
MonaBabikir <Mona.Mohamedelamin.6258@student.uu.se>
2019-03-28 16:37:21 +0100

    "learning on real data CSC"

---
+++ v4_CSC_data.py
@@ -0,0 +1,251 @@
+import glob
+import csv
+import os
+import numpy as np
+import sklearn
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+import itertools
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.preprocessing import StandardScaler , MinMaxScaler
+from keras.models import Sequential, load_model
+from keras.layers import Dense , LSTM
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from keras.callbacks import ModelCheckpoint
+
+from v4_CSC_data_preprocess import window_data
+
+## uploading dataset
+# data = pd.read_csv("./real_data_prepared/epouta/e101_epouta_csc_fi.csv", header=None)
+# data.columns = ['Time' , 'Cpu', 'Power']
+# print(list(data))
+# print(len(data))
+#
+# data['DateTime'] = pd.to_datetime(data.Time)
+#
+# data.drop(['Power', 'Time'], axis=1, inplace=True)
+# print(data.head)
+# print(data.dtypes)
+# print(data.iloc[4415].DateTime.day)
+#
+# label_data = data['Cpu']
+# in_data = data['DateTime']
+
+data = window_data("./real_data_prepared/epouta/e101_epouta_csc_fi.csv")
+label_data = data['Cpu_t+3']
+in_data = data.drop(['Cpu_t+3'], axis=1)
+
+## train data split in order
+n = int(float(data.shape[0]) * 0.8)
+train_data = in_data[:n]
+train_labels = label_data[:n]
+test_data = in_data[n:]
+test_labels = label_data[n:]
+
+## train data split in randomly
+#train_data, test_data, train_labels, test_labels = train_test_split(in_data, label_data, test_size=0.2, random_state=42)
+
+print(train_data)
+print(train_data.shape)
+
+print(test_data)
+print(test_data.shape)
+
+plt.plot(train_labels)
+plt.show()
+#
+#
+# train_data, test_data = train_test_split(data, test_size=0.2)
+# #train_target = train_data["Power"]
+# #train_data.drop(["Power"] , axis=1 , inplace=True)
+# train_data , train_target = np.hsplit(train_data, [-1])
+# #test_target = test_data["Power"]
+# #test_data.drop(["Power"] , axis=1 , inplace=True)
+# test_data , test_target = np.hsplit(test_data, [-1])
+#
+# ## min/max values for the target
+print("\n min value in the train target and test target :", np.amin(train_labels) , " , " , np.amin(test_labels))
+print("\n max value in the train target and test target :", np.amax(train_labels) , " , " , np.amax(test_labels))
+# ##
+#
+# ## data scaling
+# scaler1 = StandardScaler()
+# scaler2 = StandardScaler()
+# train_data = scaler1.fit_transform(train_data)
+# test_data = scaler1.fit_transform(test_data)
+# train_target =scaler2.fit_transform(train_target)
+# test_target = scaler2.fit_transform(test_target)
+# ## end of data scaling
+#
+#
+# # print(cpu_df.shape)
+#
+# ## end of data loading
+#
+# ## model building
+def build_model():
+    NN_model = Sequential()
+
+    NN_model.add(Dense(15, kernel_initializer='normal',input_dim = train_data.shape[1], activation='relu')) #input_dim = train_data.shape[1]
+    #NN_model.add(Dense(2, kernel_initializer='normal', activation='relu'))
+    NN_model.add(Dense(1, kernel_initializer='normal',activation='relu'))
+
+    # ## model compilation
+    NN_model.compile(loss='mean_squared_error', optimizer='adam') #'mean_absolute_error', 'mean_squared_error' , , metrics=['accuracy']
+
+    return NN_model
+
+
+NN_model = build_model()
+
+Model_Checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', mode='min', verbose=1, save_best_only=True)
+
+# ## model fitting
+model_training = NN_model.fit(train_data, train_labels, epochs=1000, batch_size=32, validation_data=(test_data, test_labels) , callbacks=[Model_Checkpoint]) #validation_split = 0.2
+# print("Data saved in history: \n", print(model_training.history.keys()))
+# print("Model Training History: \n" , model_training.history , "\n")
+#
+# ## model evaluation
+model_evaluation_test = NN_model.evaluate(test_data, test_labels, batch_size=32 , verbose=0)
+model_evaluation_train = NN_model.evaluate(train_data, train_labels, batch_size=32 , verbose=0)
+model_evaluation_alldata = NN_model.evaluate(in_data, label_data, batch_size=32 , verbose=0)
+print("trainset evaluation _on the final model: \n",NN_model.metrics_names,  model_evaluation_train,"\n")
+print("Model Evaluation (testset evaluation) _on the final model: \n",NN_model.metrics_names,  model_evaluation_test,"\n")
+print("the whole model evaluation _on the final model: \n" , NN_model.metrics_names,  model_evaluation_alldata , "\n")
+
+saved_best_model = load_model('best_model.h5')
+train_loss = saved_best_model.evaluate(train_data, train_labels, verbose=0)
+test_loss = saved_best_model.evaluate(test_data, test_labels, verbose=0)
+print("train loss evaluation _on the best model: \n",NN_model.metrics_names,  train_loss,"\n")
+print("test loss evaluation _on the best model: \n",NN_model.metrics_names,  test_loss,"\n")
+
+
+
+### using sklearn MLP
+# mlp = MLPClassifier(hidden_layer_sizes=(2,), max_iter=500, alpha=1e-4,
+#                     solver='sgd', verbose=10, tol=1e-4, random_state=1,
+#                     learning_rate_init=.1)
+# # Train the classifier
+# x_train = train_data.to_numpy()
+# y_train = train_labels.to_numpy()
+# x_test = test_data.to_numpy()
+# y_test = test_labels.to_numpy()
+#
+# mlp.fit(x_train, y_train)
+# # Record the final results for the trained data
+# print("Training set score: %f" % mlp.score(x_train, y_train))
+# # Test the classifier with the test data
+# print("Test set score: %f\n\n###################" % mlp.score(x_test, y_test))
+
+
+### evaluation using scikit-Learn cross validation (https://machinelearningmastery.com/regression-tutorial-keras-deep-learning-library-python/)
+# seed = 7
+# np.random.seed(seed)
+# # evaluate model with standardized dataset
+# estimator = KerasRegressor(build_fn=build_model, epochs=1000, batch_size=32, verbose=0)
+#
+# kfold = KFold(n_splits=10, random_state=seed)
+# scores = cross_val_score(estimator, in_data, label_data, cv=kfold)
+# #print("Results: %.10f (%.10f) MSE" % (scores.mean(), scores.std()))
+# print("Results: %.10f MSE" % (np.mean(scores)))
+# print(scores , "\n")
+
+#### with standerdize data :
+# estimators = []
+# estimators.append(('standardize', StandardScaler()))
+# estimators.append(('mlp', KerasRegressor(build_fn=build_model, epochs=500, batch_size=32, verbose=0)))
+# pipeline = Pipeline(estimators)
+# kfold = KFold(n_splits=10, random_state=seed)
+# scores = cross_val_score(pipeline, in_data, label_data, cv=kfold)
+# #print("Standardized: %.10f (%.10f) MSE" % (scores.mean(), scores.std()))
+# print("Standardized: %.10f MSE" % (np.mean(scores)))
+# print(scores , "\n")
+
+# train_predict = NN_model.predict(train_data)
+# test_predict = NN_model.predict(test_data)
+#
+# train_predict = scaler2.inverse_transform(train_predict)
+# train_y = scaler2.inverse_transform(train_target)
+#
+# test_predict = scaler2.inverse_transform(test_predict)
+# test_y = scaler2.inverse_transform(test_target)
+#
+# train_loss = mean_squared_error(train_predict , train_y)
+# test_loss = mean_squared_error(test_predict, test_y)
+#
+# print("\n train data MSE : ", train_loss)
+# print("\n test data MSE : ", test_loss)
+
+
+## some prediction
+print("\nfirst data example : ", train_data.iloc[1000] , "  ," , train_labels.iloc[1000])
+model_prediction_1 = NN_model.predict(np.array([train_data.iloc[1000]]))
+print("model prediction : ", model_prediction_1)
+
+print("\nfirst data example : ", test_data.iloc[700] , "  ," , test_labels.iloc[700])
+model_prediction_2 = NN_model.predict(np.array([test_data.iloc[700]]))
+print("model prediction : ", model_prediction_2)
+
+print("\nfirst data example : ", test_data.iloc[50] , "  ," , test_labels.iloc[50])
+model_prediction_2 = NN_model.predict(np.array([test_data.iloc[50]]))
+print("model prediction : ", model_prediction_2)
+
+
+
+
+#
+# ### neural network loss(MSE) plotting (https://machinelearningmastery.com/display-deep-learning-model-training-history-in-keras/)
+plt.plot(model_training.history['loss'] , label='train')
+plt.plot(model_training.history['val_loss'] , label='test')
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(loc='upper left')
+plt.show()
+#
+# plt.plot(model_training.history['acc'])
+# plt.plot(model_training.history['val_acc'])
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'validation'], loc='upper left')
+# plt.show()
+
+
+### plotting data shape
+#plt.plot(data['Cpu'])
+# plt.plot(data['DateTime'] , data['Cpu'])
+# plt.title("CPU Time Series")
+# plt.ylabel('CPU Reading')
+# plt.xlabel('DataTime')
+# plt.show()
+
+
+### plot training/ test prediction
+train_predict = NN_model.predict(train_data)
+test_predict = NN_model.predict(test_data)
+
+fig = plt.figure()
+plt.subplot(1,2, 1)
+plt.plot(train_labels  , label='train actual')
+plt.plot(train_predict , label='train predict')
+
+plt.subplot(1, 2,2)
+plt.plot(test_labels, label='test actual')
+plt.plot(test_predict , label="test predict")
+
+plt.title(' Actual and Predicted ')
+plt.ylabel('CPU')
+plt.xlabel('time')
+plt.legend(loc='upper left')
+plt.show()
+
+
---
+++ v4_CSC_data_preprocess.py
@@ -0,0 +1,205 @@
+import os
+import pandas as pd
+
+## data preprocessing to put all the data together
+def aggregate_data():
+    data_path = "./real_data/"
+    dir_names = os.listdir(data_path)
+    agg_data = []
+    for i in range(len(dir_names)):
+        file_path = data_path +  dir_names[i]
+        cpu_df = pd.read_csv(file_path +"/cpu.csv", delimiter=';')
+        net_df = pd.read_csv(file_path +"/net.csv", delimiter=';')
+        power_df = pd.read_csv(file_path +"/power.csv", delimiter=';')
+        data = pd.concat([cpu_df, net_df, power_df], axis=1)
+        agg_data.append(data)
+
+    agg_data = pd.concat(agg_data)
+    agg_data.to_csv("./aggregated_data.csv")
+
+#aggregate_data()
+## end of preprocessing
+
+# data = pd.read_csv("./real_data/10-27/cpu.csv" , delimiter=';')
+# print(list(data))
+# print(len(data))
+# print(len(data['Series'].unique()))
+# print((data['Series'].unique())[1].split('.'))
+# x = (data['Series'].unique())[1].split('.')[4] == (data['Series'].unique())[10].split('.')[4]
+# print(x)
+#
+#
+# print(data['Time'][0] == data['Time'][146])
+# print(len(data['Time'].unique()))
+#
+# df = pd.DataFrame(data['Series'])
+# df = df.applymap(lambda x: x.split('.')[2])
+# print(df.head)
+# print(len(df))
+#
+# data = pd.concat([df , data] ,axis=1)
+#
+# data.columns = ['Series' , 'Full_Series', 'Time', 'Value']
+# data.drop(['Full_Series'], axis=1, inplace=True)
+# group = data.groupby('Series')
+# print(list(data), "  ", len(data))
+#
+# print(group.describe())
+#
+# for series, df_series in data.groupby('Series'):
+#     print(series)
+#     #print(df_series)
+#     agg_df = df_series.groupby(by='Time')['Value'].sum()
+#     print(len(agg_df))
+#
+#     with open('./real_data_prepared/' + series + '.csv' , 'a') as f:
+#         agg_df.to_csv(f, header=False)
+
+
+
+def onefile_prepare(cpu_path, power_path):
+    #cpu_data = pd.read_csv("./real_data/10-27/cpu.csv" , delimiter=';')
+    #power_data = pd.read_csv("./real_data/10-27/power.csv", delimiter=';')
+    cpu_data = pd.read_csv(cpu_path, delimiter=';')
+    power_data = pd.read_csv(power_path, delimiter=';')
+
+    #print(list(data))
+    #print(len(data))
+
+    cpu_df = pd.DataFrame(cpu_data['Series'])
+    power_df = pd.DataFrame(power_data['Series'])
+
+    cpu_df = cpu_df.applymap(lambda x: x.split('.')[2])
+    power_df = power_df.applymap(lambda x: x.split('.')[2])
+    #print(df.head)
+    #print(len(df))
+
+    cpu_data = pd.concat([cpu_df, cpu_data], axis=1)
+    power_data = pd.concat([power_df, power_data], axis=1)
+
+    cpu_data.columns = ['Series', 'Full_Series', 'Time', 'Value']
+    cpu_data.drop(['Full_Series'], axis=1, inplace=True)
+
+    power_data.columns = ['Series', 'Full_Series', 'Time', 'Value']
+    power_data.drop(['Full_Series'], axis=1, inplace=True)
+
+    cpu_data_list = []
+    for series, df_series in cpu_data.groupby('Series'):
+        #print(series)
+        # print(df_series)
+        agg_df = df_series.groupby(by='Time')['Value'].sum()
+        #print(len(agg_df))
+
+        cpu_data_list.append(agg_df)
+        # with open('./real_data_prepared/power/' + series + '.csv', 'a') as f:
+        #     agg_df.to_csv(f, header=False)
+
+    power_data_list = []
+    series_list = []
+    for series, df_series in power_data.groupby('Series'):
+        #print(series)
+        series_list.append(series)
+        # print(df_series)
+        agg_df = df_series.groupby(by='Time')['Value'].sum()
+        #print(len(agg_df))
+
+        power_data_list.append(agg_df)
+        # with open('./real_data_prepared/power/' + series + '.csv', 'a') as f:
+        #     agg_df.to_csv(f, header=False)
+
+    if len(cpu_data_list) == len(power_data_list) == len(series_list):
+        for i in range(len(cpu_data_list)):
+            c_p_agg_df = pd.concat([cpu_data_list[i] , power_data_list[i]], axis=1)
+            #print("length of aggregated = ", len(c_p_agg_df))
+            with open('./real_data_prepared/epouta/' + series_list[i] + '.csv', 'a') as f:
+                c_p_agg_df.to_csv(f, header=False)
+
+
+    else:
+        print("\nSorry there is a problems with the files :( \n")
+
+
+def allfiles_prepare():
+    data_path = "./real_data/epouta/"
+    dir_names = os.listdir(data_path)
+    for i in range(len(dir_names)):
+        print(" In directory : ", dir_names[i])
+        file_path = data_path +  dir_names[i]
+        cpu_path = file_path + "/cpu.csv"
+        power_path = file_path +"/power.csv"
+        net_path = file_path +"/net.csv"
+
+        onefile_prepare(cpu_path, power_path)
+
+
+#allfiles_prepare()
+#test = pd.read_csv("./real_data/10-27/net.csv" , delimiter=';')
+
+def netfiles_prepare(): ## to be combined later with allfiles_prepare
+    data_path = "./real_data/epouta/"
+    dir_names = os.listdir(data_path)
+    for i in range(len(dir_names)):
+        print(" In directory : ", dir_names[i])
+        file_path = data_path + dir_names[i]
+        net_path = file_path +"/net.csv"
+
+        net_data = pd.read_csv(net_path ,delimiter=';') #, dtype={2:'float64'}
+        print(len(net_data))
+        print(list(net_data))
+        #print(test.iloc[12587])
+        value_df = pd.DataFrame(net_data['Value'])
+        print(value_df.iloc[12829])
+        def r(x):
+            x = str(x)
+            x = x.replace(',', '')
+            x= float(x)
+            #print(x)
+            return x
+
+        value_df = value_df.applymap(lambda x: r(x))
+        print(value_df.iloc[12829])
+
+        net_data = pd.concat([net_data , value_df] ,axis=1)
+        net_data.columns = ['Series' , 'Time' , 'OldValue' , 'Value']
+        net_data.drop(['OldValue'] , axis=1 , inplace=True)
+
+        net_series_df = pd.DataFrame(net_data['Series'])
+
+        net_series_df = net_series_df.applymap(lambda x: x.split('.')[0])
+
+        net_data = pd.concat([net_series_df, net_data], axis=1)
+
+        net_data.columns = ['Series', 'Full_Series', 'Time', 'Value']
+        net_data.drop(['Full_Series'], axis=1, inplace=True)
+
+        print(list(net_data))
+        #print(net_data.head)
+
+        for series, df_series in net_data.groupby('Series'):
+            # print(series)
+            # print(df_series)
+            agg_df = df_series.groupby(by='Time')['Value'].sum()
+            # print(len(agg_df))
+
+            with open('./real_data_prepared/epouta/net/' + series + '.csv', 'a') as f:
+                 agg_df.to_csv(f, header=False)
+
+
+#netfiles_prepare()
+
+
+def window_data(file_path):
+    #data = pd.read_csv("./real_data_prepared/epouta/e101_epouta_csc_fi.csv", header=None)
+    data = pd.read_csv(file_path, header=None)
+    data.columns = ['Time', 'Cpu_t', 'Power']
+    data.drop(['Power' , 'Time'] , axis=1 , inplace=True)
+    data['Cpu_t+1'] = data['Cpu_t'].shift(-1)
+    data['Cpu_t+2'] = data['Cpu_t+1'].shift(-1)
+    data['Cpu_t+3'] = data['Cpu_t+2'].shift(-1)
+    data.dropna(inplace=True)
+    #print(data)
+    return data
+
+window_data("./real_data_prepared/epouta/e101_epouta_csc_fi.csv")
+
+
